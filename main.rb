543d6d79bee835521df1ca7f2ae51d22b4379d36
harkato <92404938+harkato@users.noreply.github.com>
2023-08-29 22:08:25 -0300

    Finish stock picker

--- main.rb
+++ main.rb
@@ -1,14 +1,23 @@
-array = [17,3,6,9,15,8,6,1,10]
+prices = [17,3,6,9,15,8,6,1,1]
 
 
-def stock_picker(array)
-    max = array.max(2)
-    min = array.min(2)
-    values = [max, min].flatten
+def stock_picker(prices)
+  best_buy_day, best_sell_day = 0
+  best_profit = 0
+  min_price = prices[0]
+
+  prices.each_with_index do |price, day|
+    if price < min_price
+      min_price = price
+    elsif price - min_price > best_profit
+      best_profit = price - min_price
+      best_buy_day = min_price
+      best_sell_day = price 
+    end
+  end
+  sell_day = prices.index(best_sell_day)
+  buy_day = prices.index(best_buy_day)
+  [buy_day, sell_day]
 end
 
-def day_checker(array, days)
-    # dia da compra antes da venda
-
-
-puts stock_picker(array)
+puts stock_picker(prices)
